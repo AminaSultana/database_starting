68bcfd0e21547c5d4adb3c6b40b68520e3b9e6b0
AminaSultana <lilisultana53@gmail.com>
2022-04-30 00:25:05 +0530

    Task 7

--- src/App.js
+++ src/App.js
@@ -5,10 +5,15 @@ import "./App.css";
 
 function App() {
   const [getData, setGetData] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
   async function fetchMovieHandler(){
+    setIsLoading(true)
     const response = await fetch("https://jsonplaceholder.typicode.com/users")
     const todosArray = await response.json()
-    setGetData(todosArray)
+    if(todosArray.length>0){
+      setGetData(todosArray)
+      setIsLoading(false)
+    }
     //return todosArray
   }
   //fetchMovieHandler().then(data=> setGetData(data))
@@ -18,7 +23,8 @@ function App() {
         <button onClick={fetchMovieHandler}>Fetch Movies</button>
       </section>
       <section>
-        <MoviesList movies={getData} />
+        {isLoading && <p>Loading...</p>}
+        {!isLoading && <MoviesList movies={getData} />}
       </section>
     </React.Fragment>
   );
